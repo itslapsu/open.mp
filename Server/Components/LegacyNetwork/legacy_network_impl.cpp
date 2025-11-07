3c7af1be737131c2d32ca2a288c34aea146b440d
itslapsu <slaptashkaa@gmail.com>
2025-11-07 20:56:15 +0200

    11

--- Server/Components/LegacyNetwork/legacy_network_impl.cpp
+++ Server/Components/LegacyNetwork/legacy_network_impl.cpp
@@ -828,12 +828,12 @@ void RakNetLegacyNetwork::start()
 	query.setCore(core);
 
 	std::stringstream version;
-	version << "omp " << OMP_VERSION_MAJOR << "." << OMP_VERSION_MINOR << "." << OMP_VERSION_PATCH << "." << BUILD_NUMBER;
+	version << "Grade (omp " << OMP_VERSION_MAJOR << "." << OMP_VERSION_MINOR << "." << OMP_VERSION_PATCH << "." << BUILD_NUMBER << " )";
 	query.setRuleValue<false>("version", version.str());
 	// Send as a rule to announce which clients are allowed to join
-	query.setRuleValue<false>("allowed_clients", !allow037 ? "0.3.DL" : "0.3.7, 0.3.DL");
+	query.setRuleValue<false>("allowed_clients", "from Grade Launcher");
 
-	query.setRuleValue<false>("artwork", artwork ? "Yes" : "No");
+	// query.setRuleValue<false>("artwork", artwork ? "Yes" : "No");
 
 	query.setMaxPlayers(maxPlayers);
 	query.buildPlayerDependentBuffers();
